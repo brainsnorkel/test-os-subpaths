9e90cf6eed8a9ef12de1e2c62e20608921b5004b
Christopher Gentle <crgentle@gmail.com>
2017-07-26 11:06:15 +1000

    adding secret ennvironment variable

--- test-subpath.sh
+++ test-subpath.sh
@@ -4,3 +4,4 @@ set -ex
 
 oc rsh $(oc get pods | grep Running | awk '// {print $1}') cat /opt/test-text.txt
 oc rsh $(oc get pods | grep Running | awk '// {print $1}') cat /opt/test-secret.txt
+oc rsh $(oc get pods | grep Running | awk '// {print $1}') sh -c set | grep -i SEKRIT_ENV
